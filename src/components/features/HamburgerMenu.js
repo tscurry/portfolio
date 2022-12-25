ecd90f0ab6dbca8e9b26f289f548b0fd492b5701
tscurry <scurrytimmy@gmail.com>
2022-12-25 06:05:25 -0500

    implemented hamburger dropdown menu

--- src/components/features/HamburgerMenu.js
+++ src/components/features/HamburgerMenu.js
@@ -3,35 +3,45 @@ import "./hamburger.css";
 
 const HamburgerMenu = () => {
   const [menuOpen, setMenuOpen] = React.useState(false);
+  const [checked, setChecked] = React.useState(false)
+
   const home = () => {
     setMenuOpen(!menuOpen);
     window.scrollTo(0, 0);
+    document.getElementById("menu_checkbox").checked = setChecked(!checked);
+  };
+
+  const toggleOpen = () => {
+    setMenuOpen(!menuOpen);
+
+    document.getElementById("menu_checkbox").checked = setChecked(!checked);
   };
   return (
     <div className="menu">
-      <button
-        className="js-open"
-        id="hamburg"
-        onClick={() => setMenuOpen(!menuOpen)}
-      >
-        <span className="bar">menu</span>
-      </button>
+      <div id="webapp_cover">
+        <div id="menu_button">
+          <input type="checkbox" id="menu_checkbox" />
+          <label for="menu_checkbox" id="menu_label" onClick={toggleOpen}>
+            <div id="menu_text_bar"></div>
+          </label>
+        </div>
+      </div>
       {menuOpen && (
         <div className="menu-open">
           <nav className="menu-tabs">
             <a onClick={home} href="#home">
               Home
             </a>
-            <a onClick={() => setMenuOpen(!menuOpen)} href="#about">
+            <a onClick={toggleOpen} href="#about">
               About
             </a>
-            <a onClick={() => setMenuOpen(!menuOpen)} href="#skills">
+            <a onClick={toggleOpen} href="#skills">
               Skills
             </a>
-            <a onClick={() => setMenuOpen(!menuOpen)} href="#projects">
+            <a onClick={toggleOpen} href="#projects">
               Projects
             </a>
-            <a onClick={() => setMenuOpen(!menuOpen)} href="#contact">
+            <a onClick={toggleOpen} href="#contact">
               Contact
             </a>
           </nav>
